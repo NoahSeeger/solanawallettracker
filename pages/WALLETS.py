87d2fb2cd7eea62e24172ce8742107d6c8b2879f
NoahSeeger <cuzimnoah1@gmail.com>
2024-04-02 11:21:37 +0200

    test

--- pages/WALLETS.py
+++ pages/WALLETS.py
@@ -1,20 +1,15 @@
 import gspread
 import streamlit as st
 
-gc = gspread.service_account_from_dict({
-    "type": st.secrets["google"]["type"],
-    "project_id": st.secrets["google"]["project_id"],
-    "private_key_id": st.secrets["google"]["private_key_id"],
-    "private_key": st.secrets["google"]["private_key"],
-    "client_email": st.secrets["google"]["client_email"],
-    "client_id": st.secrets["google"]["client_id"],
-    "auth_uri": st.secrets["google"]["auth_uri"],
-    "token_uri": st.secrets["google"]["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["google"]["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["google"]["client_x509_cert_url"],
-    "universe_domain": st.secrets["google"]["universe_domain"]
-})
+gc = gspread.service_account_from_dict(st.secrets["google"])
 sh = gc.open("Crypto Wallets")
-sh = gc.open("Crypto Wallets")
+worksheet = sh.sheet1
 
-st.write(sh.sheet1.get('A1'))
+first_names = ["John", "Emma", "Mark"]
+last_names = ["Doe", "Smith", "Johnson"]
+
+for i in range(len(first_names)):
+    row = [first_names[i], last_names[i]]
+    worksheet.append_row(row)
+
+st.write("Daten wurden erfolgreich hinzugefügt!")
