216c77d3a06c703692e4599d08a1ca9474298da5
NoahSeeger <cuzimnoah1@gmail.com>
2024-04-02 12:31:23 +0200

    test

--- SOLANA.py
+++ SOLANA.py
@@ -33,11 +33,16 @@ tab1,tab2 = st.tabs(["Input Address", "Select from Data"])
 
 with tab1:
    wallet = st.text_input(label="Wallet-Adress",placeholder="e.g. 71WDyyCsZwyEYDV91Qrb212rdg6woCHYQhFnmZUBxiJ6", max_chars=50, disabled=False)
-   
+
 with tab2:
    wallet_select = st.selectbox('Select from saved Addresses', api_handler.get_wallet_addresses(), index=None)
 
 button = st.button("Search 🔍")
+
+if tab1:
+   st.write("TAB 1")
+else:
+   st.write("TAB NOT")
 
 if button:
     if wallet != "" and len(wallet) >= 5:
